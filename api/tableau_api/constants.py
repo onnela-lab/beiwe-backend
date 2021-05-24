276895422e20b35fb5504ac747d9e63b118348aa
Alvin Siu <alvin@zagaran.com>
2021-05-24 17:36:37 -0400

    Fix Summary Statistics serializer

--- api/tableau_api/constants.py
+++ api/tableau_api/constants.py
@@ -33,6 +33,7 @@ SERIALIZABLE_FIELD_NAMES = [
     "sleep_duration",
     "sleep_onset_time",
     "stationary_fraction",
+    "study_id",
     "text_incoming_count",
     "text_incoming_degree",
     "text_incoming_length",
