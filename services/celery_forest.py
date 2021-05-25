092f20a9e1ed29117c0f6d37c186d9e532d9a6be
Alvin Siu <alvin@zagaran.com>
2021-05-25 12:46:16 -0400

    Rename tracker to task

--- services/celery_forest.py
+++ services/celery_forest.py
@@ -32,57 +32,57 @@ TREE_TO_FOREST_FUNCTION = {
 
 
 def create_forest_celery_tasks():
-    pending_trackers = ForestTask.objects.filter(status=ForestTask.Status.queued)
+    pending_tasks = ForestTask.objects.filter(status=ForestTask.Status.queued)
 
     # with make_error_sentry(sentry_type=SentryTypes.data_processing):  # add a new type?
     with NullErrorHandler():  # for debugging, does not suppress errors
-        for tracker in pending_trackers:
-            print(f"Queueing up celery task for {tracker.participant} on tree {tracker.forest_tree} from {tracker.data_date_start} to {tracker.data_date_end}")
-            enqueue_forest_task(args=[tracker.id])
+        for task in pending_tasks:
+            print(f"Queueing up celery task for {task.participant} on tree {task.forest_tree} from {task.data_date_start} to {task.data_date_end}")
+            enqueue_forest_task(args=[task.id])
 
 
 #run via celery as long as tasks exist
 @forest_celery_app.task(queue=FOREST_QUEUE)
 def celery_run_forest(forest_task_id):
     with transaction.atomic():
-        tracker = ForestTask.objects.filter(id=forest_task_id).first()
+        task = ForestTask.objects.filter(id=forest_task_id).first()
 
-        participant = tracker.participant
-        forest_tree = tracker.forest_tree
+        participant = task.participant
+        forest_tree = task.forest_tree
         
         # Check if there already is a running task for this participant and tree, handling
         # concurrency and requeuing of the ask if necessary
-        trackers = (
+        tasks = (
             ForestTask
                 .objects
                 .select_for_update()
                 .filter(participant=participant, forest_tree=forest_tree)
         )
-        if trackers.filter(status=ForestTask.Status.running).exists():
-            enqueue_forest_task(args=[tracker.id])
+        if tasks.filter(status=ForestTask.Status.running).exists():
+            enqueue_forest_task(args=[task.id])
             return
         
-        # Get the chronologically earliest tracker that's queued
-        tracker = (
-            trackers
+        # Get the chronologically earliest task that's queued
+        task = (
+            tasks
                 .filter(status=ForestTask.Status.queued)
                 .order_by("-data_date_start")
                 .first()
         )
-        if tracker is None:
+        if task is None:
             return
         
-        # Set metadata on the tracker
-        tracker.status = ForestTask.Status.running
-        tracker.forest_version = get_distribution("forest").version
-        tracker.process_start_time = timezone.now()
-        tracker.save(update_fields=["status", "forest_version", "process_start_time"])
+        # Set metadata on the task
+        task.status = ForestTask.Status.running
+        task.forest_version = get_distribution("forest").version
+        task.process_start_time = timezone.now()
+        task.save(update_fields=["status", "forest_version", "process_start_time"])
 
     try:
         # Save file size data
         # The largest UTC offsets are -12 and +14
-        min_datetime = datetime.combine(tracker.data_date_start, datetime.min.time()) - timedelta(hours=12)
-        max_datetime = datetime.combine(tracker.data_date_end, datetime.max.time()) + timedelta(hours=14)
+        min_datetime = datetime.combine(task.data_date_start, datetime.min.time()) - timedelta(hours=12)
+        max_datetime = datetime.combine(task.data_date_end, datetime.max.time()) + timedelta(hours=14)
         chunks = (
             ChunkRegistry
                 .objects
@@ -90,40 +90,40 @@ def celery_run_forest(forest_task_id):
                 .filter(time_bin__gte=min_datetime)
                 .filter(time_bin__lte=max_datetime)
         )
-        tracker.total_file_size = chunks.aggregate(Sum('file_size')).get('file_size__sum')
-        tracker.save(update_fields=["total_file_size"])
+        task.total_file_size = chunks.aggregate(Sum('file_size')).get('file_size__sum')
+        task.save(update_fields=["total_file_size"])
     
         # Download data
-        create_local_data_files(tracker, chunks)
-        tracker.process_download_end_time = timezone.now()
-        tracker.save(update_fields=["process_download_end_time"])
+        create_local_data_files(task, chunks)
+        task.process_download_end_time = timezone.now()
+        task.save(update_fields=["process_download_end_time"])
 
         # Run Forest
-        params_dict = tracker.params_dict()
-        tracker.params_dict_cache = json.dumps(params_dict, cls=DjangoJSONEncoder)
-        tracker.save(update_fields=["params_dict_cache"])
-        TREE_TO_FOREST_FUNCTION[tracker.forest_tree](**params_dict)
+        params_dict = task.params_dict()
+        task.params_dict_cache = json.dumps(params_dict, cls=DjangoJSONEncoder)
+        task.save(update_fields=["params_dict_cache"])
+        TREE_TO_FOREST_FUNCTION[task.forest_tree](**params_dict)
         
         # Save data
-        tracker.forest_output_exists = tracker.construct_summary_statistics()
-        tracker.save(update_fields=["forest_output_exists"])
-        save_cached_files(tracker)
+        task.forest_output_exists = task.construct_summary_statistics()
+        task.save(update_fields=["forest_output_exists"])
+        save_cached_files(task)
     except Exception:
-        tracker.status = tracker.Status.error
-        tracker.stacktrace = traceback.format_exc()
+        task.status = task.Status.error
+        task.stacktrace = traceback.format_exc()
     else:
-        tracker.status = tracker.Status.success
-    tracker.clean_up_files()
-    tracker.process_end_time = timezone.now()
-    tracker.save()
+        task.status = task.Status.success
+    task.clean_up_files()
+    task.process_end_time = timezone.now()
+    task.save()
     
 
 
-def create_local_data_files(tracker, chunks):
+def create_local_data_files(task, chunks):
     for chunk in chunks.values("study__object_id", *chunk_fields):
         contents = s3_retrieve(chunk["chunk_path"], chunk["study__object_id"], raw_path=True)
         file_name = os.path.join(
-            tracker.data_input_path,
+            task.data_input_path,
             determine_file_name(chunk),
         )
         os.makedirs(os.path.dirname(file_name), exist_ok=True)
@@ -143,10 +143,10 @@ def enqueue_forest_task(**kwargs):
     safe_apply_async(celery_run_forest, **updated_kwargs)
 
 
-def save_cached_files(tracker):
-    if os.path.exists(tracker.all_bv_set_path):
-        with open(tracker.all_bv_set_path, "rb") as f:
-            tracker.save_all_bv_set_bytes(f.read())
-    if os.path.exists(tracker.all_memory_dict_path):
-        with open(tracker.all_memory_dict_path, "rb") as f:
-            tracker.save_all_memory_dict_bytes(f.read())
+def save_cached_files(task):
+    if os.path.exists(task.all_bv_set_path):
+        with open(task.all_bv_set_path, "rb") as f:
+            task.save_all_bv_set_bytes(f.read())
+    if os.path.exists(task.all_memory_dict_path):
+        with open(task.all_memory_dict_path, "rb") as f:
+            task.save_all_memory_dict_bytes(f.read())
