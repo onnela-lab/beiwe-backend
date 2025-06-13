ab199109d5605ba3a19ded7d2f02bc34b11d5941
Eli Jones <biblicabeebli@gmail.com>
2025-06-13 05:31:05 -0400

    [staged-updates] adds another concurrently addable migration...

--- database/profiling_models.py
+++ database/profiling_models.py
@@ -277,7 +277,10 @@ class UploadTracking(UtilityModel):
 class S3File(TimestampedModel):
     
     class Meta:  # type: ignore
-        indexes = [models.Index(fields=["created_on"], name="s3file_created_on_idx"), ]
+        indexes = [
+            models.Index(fields=["created_on"], name="s3file_created_on_idx"),
+            models.Index(fields=["sha1"], name="s3file_sha1_idx"),
+        ]
     
     study: Study = models.ForeignKey("Study", on_delete=models.PROTECT, null=True, blank=True, related_name="s3_files")
     participant: Participant = models.ForeignKey("Participant", on_delete=models.PROTECT, null=True, blank=True, related_name="s3_files")
