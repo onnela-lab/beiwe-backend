620fdca643ccf90de1f9e384f8a6f153d52cee1d
Eli Jones <biblicabeebli@gmail.com>
2025-07-18 14:37:51 -0400

    [fix-metadata-for-chunks-s3storage] does trivial conversion of some deprecated functions

--- libs/file_processing/utility_functions_csvs.py
+++ libs/file_processing/utility_functions_csvs.py
@@ -1,7 +1,7 @@
 import re
 from datetime import datetime
 
-from constants.common_constants import API_TIME_FORMAT
+from constants.common_constants import API_TIME_FORMAT, UTC
 
 
 def insert_timestamp_single_row_csv(header: bytes, rows_list: list[list], time_stamp: bytes) -> bytes:
@@ -127,4 +127,4 @@ def construct_csv_as_bytes(header: bytes, rows_list: list[list[bytes]]) -> bytes
 
 
 def unix_time_to_string(unix_time: int) -> bytes:
-    return datetime.utcfromtimestamp(unix_time).strftime(API_TIME_FORMAT).encode()
+    return datetime.fromtimestamp(unix_time, UTC).strftime(API_TIME_FORMAT).encode()
