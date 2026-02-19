45c080640fd3260f66ffbed0c9f3c30edcba59dc
Eli Jones <biblicabeebli@gmail.com>
2026-02-18 23:20:02 -0500

    [file-processing-updates] found a bug during file processing updates in junk file removal

--- scripts/script_that_deletes_known_junk_uploads.py
+++ scripts/script_that_deletes_known_junk_uploads.py
@@ -4,7 +4,8 @@ from time import perf_counter
 
 from django.utils import timezone
 
-from database.models import S3File
+from constants.data_stream_constants import SURVEY_ANSWERS
+from database.models import ChunkRegistry, S3File
 from libs.s3 import s3_delete
 
 
@@ -152,74 +153,76 @@ for y in x:
 # on 12 files from 2019 - which is probably a since-fixed old bug - but these have a small number of 
 # files so it should be fine to disable deleting them.  teeny tiny storage cost.
 sha1_hashes = [
-# "21f38aedd23cbb507f185303cc914880fb62ec6f", survey answers
-# "acbd957a28d8833567be73376a20e27bcffa1b19", survey answers
-# "2701ea94a0a8578b480cd69903b958aaaa2ad369", survey timings
-# "2cb0183eab9e8bfcd99c4e9ac8ee304cd912eb0c", survey timings
-# "3532fbaea0361e2e26f5d5023c95398520fdc33c", survey timings
-# "47f1e17a51a5a4bbf9cadb4fe7829645745b931c", survey timings
-# "7dbf867f2f35693d69a4bf0c36ffb21f8e13f81a", survey timings
-# "8f111b9d55e88f062bbd1e18f959bad93ea681ba", survey timings
-# "a76efa6915a7545e5176a33beb770b3068c603c9", survey timings
-# "d7c0170ea85bb05fb961195259209528e1cb4e8b", survey timings
-"01db54f4d3629ec1df464551489dbd03516dce95",
-"03879618e5d85666bdec52b95d4fef77b2b1b8a1",
-"04945d8ef2bbfaa008a05a1354b2cfa0fcef9312",
-"072ef7b9505993de878de556551eea87cb331ed4",
-"0f7f1c13cc73ffe20e83eee2f62749e078377730",
-"0ffe6caff593dd99067a61799f6338367b0c374d",
-"1184ef4c7f8fdffb19a555a596a05cdfa10dae15",
-"127b36b8633f7f0407f16705d52f3d76a3f2e5d9",
-"1cf33194c4e2d8e951ac86f26c4aaceac9afc996",
-"289415afb9270b622385e450b53289a5c514db27",
-"296ba5b840b1ef1b9e30e57274a06f6f1d878dd2",
-"2e73a0f212ad4c8e4871189251e13e50eaf4d420",
-"346886aac2f9c19f790ed6213ba07d291a5aa985",
-"3a56cd0133bc2cb707b176cde922b5108892a71a",
-"3c1d21995db3f94521ca38731bd6a6b1f2b6f193",
-"3c23292f17c1d6185f5a46024afc9619986c5496",
-"422647432f8e019243f68aa482488eba9f21f203",
-"49799b5498e73789c094bd5e92e2e606e8df3919",
-"4992aa194a1477503e07db4b3a34b9c3969337a4",
-"4db27728d1ff6b0bccc6fe1c17be613a78e1d1ca",
-"4e43222cfe9e42a83ee9b27dd9efd9cfbe935f90",
-"574e43f4fd9b8d36b1a70ed5255a70446c6fddb8",
-"59ef78a196314e0e8f4a01ef8beefb1c5df6355b",
-"61946222edd04666c9a0d66d3b30fcfed55aa8fd",
-"61c8fcd0e352e1850cd184713dccaafebee3156f",
-"74e4cf4e6c20665cf55f0497866ed915102b29ed",
-"77c83b34a9bfcd97441d465dd5640510f42d7644",
-"793e67f098d855479b3020b600739ce0d7bc5b57",
-"8a0b1171eff34c1de397f2a737e89148395e0446",
-"8aee8136c16597aff010f3c174b8458e2d740233",
-"8b20061b68160dedef1e1869f91aae140093cfa2",
-"8eca9ab629212f785a4cf8831feacd41facd7c92",
-"91a122c1864b62fee435e39da1e6cbacf602c5af",
-"9347a7707d92c0866612659696eb92f33a894861",
-"951b31afcb3e17df8782b0b6e84356137f589bfc",
-"96119fae09cdeb6daaf190a049bbac80e95e522e",
-"9773475afbaa07fc4beebafc42ea3910bb7a2647",
-"a4b317e5002e32bf0a6fb805a31ccae90b044a31",
-"a4d9424dc9db68b432a146587ea9b5a7e737f155",
-"a791ace733887df6b5d7aa20e7d452b4f2e6c884",
-"ade9a92f3f8f3a54deb1641ae9276093088015d9",
-"ba6de155b4028f2c542473f1a75b21a54f017a1a",
-"c0e8fff8e2e00ec90ad0c69e87d6a0803338d699",
-"c6bf9bb3f8a0ae6a4286df524af97cc582be5a8a",
-"ce58138419ab256f2b52afa6cc32ea0f95b21232",
-"d4f2302bca18ed9d54b37bb4f08b36688dfbfd1b",
-"d74c3abd45efefa11ea95bc0987f58ac549a081a",
-"deb4da78c8018c2ae7625bf2500912a057123753",
-"e6cddda5d6e89f920cf29491f46922ac6a6214a6",
-"f15863796c0d4d6315213e76a49ae415fbb48de0",
-"f89842a9391851eb037b97ab0b0cc6076d0f2656",
-"ff77f79cd8d6b46eda2a3511fc6663e97f0db1ea",
+    # "21f38aedd23cbb507f185303cc914880fb62ec6f", survey answers
+    # "acbd957a28d8833567be73376a20e27bcffa1b19", survey answers
+    # "2701ea94a0a8578b480cd69903b958aaaa2ad369", survey timings
+    # "2cb0183eab9e8bfcd99c4e9ac8ee304cd912eb0c", survey timings
+    # "3532fbaea0361e2e26f5d5023c95398520fdc33c", survey timings
+    # "47f1e17a51a5a4bbf9cadb4fe7829645745b931c", survey timings
+    # "7dbf867f2f35693d69a4bf0c36ffb21f8e13f81a", survey timings
+    # "8f111b9d55e88f062bbd1e18f959bad93ea681ba", survey timings
+    # "a76efa6915a7545e5176a33beb770b3068c603c9", survey timings
+    # "d7c0170ea85bb05fb961195259209528e1cb4e8b", survey timings
+    "01db54f4d3629ec1df464551489dbd03516dce95",
+    "03879618e5d85666bdec52b95d4fef77b2b1b8a1",
+    "04945d8ef2bbfaa008a05a1354b2cfa0fcef9312",
+    "072ef7b9505993de878de556551eea87cb331ed4",
+    "0f7f1c13cc73ffe20e83eee2f62749e078377730",
+    "0ffe6caff593dd99067a61799f6338367b0c374d",
+    "1184ef4c7f8fdffb19a555a596a05cdfa10dae15",
+    "127b36b8633f7f0407f16705d52f3d76a3f2e5d9",
+    "1cf33194c4e2d8e951ac86f26c4aaceac9afc996",
+    "289415afb9270b622385e450b53289a5c514db27",
+    "296ba5b840b1ef1b9e30e57274a06f6f1d878dd2",
+    "2e73a0f212ad4c8e4871189251e13e50eaf4d420",
+    "346886aac2f9c19f790ed6213ba07d291a5aa985",
+    "3a56cd0133bc2cb707b176cde922b5108892a71a",
+    "3c1d21995db3f94521ca38731bd6a6b1f2b6f193",
+    "3c23292f17c1d6185f5a46024afc9619986c5496",
+    "422647432f8e019243f68aa482488eba9f21f203",
+    "49799b5498e73789c094bd5e92e2e606e8df3919",
+    "4992aa194a1477503e07db4b3a34b9c3969337a4",
+    "4db27728d1ff6b0bccc6fe1c17be613a78e1d1ca",
+    "4e43222cfe9e42a83ee9b27dd9efd9cfbe935f90",
+    "574e43f4fd9b8d36b1a70ed5255a70446c6fddb8",
+    "59ef78a196314e0e8f4a01ef8beefb1c5df6355b",
+    "61946222edd04666c9a0d66d3b30fcfed55aa8fd",
+    "61c8fcd0e352e1850cd184713dccaafebee3156f",
+    "74e4cf4e6c20665cf55f0497866ed915102b29ed",
+    "77c83b34a9bfcd97441d465dd5640510f42d7644",
+    "793e67f098d855479b3020b600739ce0d7bc5b57",
+    "8a0b1171eff34c1de397f2a737e89148395e0446",
+    "8aee8136c16597aff010f3c174b8458e2d740233",
+    "8b20061b68160dedef1e1869f91aae140093cfa2",
+    "8eca9ab629212f785a4cf8831feacd41facd7c92",
+    "91a122c1864b62fee435e39da1e6cbacf602c5af",
+    "9347a7707d92c0866612659696eb92f33a894861",
+    "951b31afcb3e17df8782b0b6e84356137f589bfc",
+    "96119fae09cdeb6daaf190a049bbac80e95e522e",
+    "9773475afbaa07fc4beebafc42ea3910bb7a2647",
+    "a4b317e5002e32bf0a6fb805a31ccae90b044a31",
+    "a4d9424dc9db68b432a146587ea9b5a7e737f155",
+    "a791ace733887df6b5d7aa20e7d452b4f2e6c884",
+    "ade9a92f3f8f3a54deb1641ae9276093088015d9",
+    "ba6de155b4028f2c542473f1a75b21a54f017a1a",
+    "c0e8fff8e2e00ec90ad0c69e87d6a0803338d699",
+    "c6bf9bb3f8a0ae6a4286df524af97cc582be5a8a",
+    "ce58138419ab256f2b52afa6cc32ea0f95b21232",
+    "d4f2302bca18ed9d54b37bb4f08b36688dfbfd1b",
+    "d74c3abd45efefa11ea95bc0987f58ac549a081a",
+    "deb4da78c8018c2ae7625bf2500912a057123753",
+    "e6cddda5d6e89f920cf29491f46922ac6a6214a6",
+    "f15863796c0d4d6315213e76a49ae415fbb48de0",
+    "f89842a9391851eb037b97ab0b0cc6076d0f2656",
+    "ff77f79cd8d6b46eda2a3511fc6663e97f0db1ea",
 ]
 
 
 def main():
     pool = ThreadPool(processes=20)
     one_week_ago = timezone.now() - timedelta(days=7)
+    one_week_ago + timedelta()
+        
     
     t1 = perf_counter()
     
@@ -235,8 +238,14 @@ def main():
                 if i % 10_000 == 0 and i > 0:
                     print(f"deleted {i} files so far...")
             
-            # turns out this can detect files that were just uploaded. oops!
+            # turns out we sometimes detect files that were just uploaded, those need to go through
+            # processing first.  Filtered by creation time of one week, long but whatever.
             S3File.fltr(path__in=paths, created_on__lt=one_week_ago).delete()
+            
+            # we need to handle any items that come up that are survey answers and are in the chunk
+            # registry, because those are going to be yoinked soon.
+            survey_paths = [path for path in paths if SURVEY_ANSWERS in path]
+            ChunkRegistry.fltr(path__in=survey_paths).delete()
         
         print(f"found and deleted {i} files total. (Current runtime: {perf_counter() - t1:.2f} seconds)")
     
